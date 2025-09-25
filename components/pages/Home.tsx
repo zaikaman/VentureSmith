50a2637ce47605ad51b3640582441bf176f8b8fc
zaikaman <91252891+zaikaman@users.noreply.github.com>
2025-09-25 22:37:08 +0700

    Removed the reveal effect on homepage

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -7,8 +7,6 @@ import { IdeaInputForm } from './IdeaInputForm';
 
 import { ScrollAndHighlightProvider } from '../../contexts/ScrollAndHighlightContext'; // Import the provider
 
-import { Reveal } from '../Reveal'; // Import Reveal component
-
 export const Home: React.FC = () => {
     const navigate = useNavigate();
 
@@ -19,15 +17,9 @@ export const Home: React.FC = () => {
     return (
         <ScrollAndHighlightProvider>
             <IdeaInputForm onGenerate={handleGenerate} />
-            <Reveal delay={200}> {/* Add a slight delay for sequential animation */}
-                <InfoSection />
-            </Reveal>
-            <Reveal delay={400}>
-                <GrowthSection />
-            </Reveal>
-            <Reveal delay={600}>
-                <TransformSection />
-            </Reveal>
+            <InfoSection />
+            <GrowthSection />
+            <TransformSection />
         </ScrollAndHighlightProvider>
     );
 };
